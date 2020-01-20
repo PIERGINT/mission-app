887cc24c73502b5fcb5b4ac933996d0e2124be7b
Pierre <p.marget@yahoo.com>
2020-01-20 15:22:46 +0100

    Springboot TP1 - Mission Lister

---
+++ src/main/java/dev/mission/exec/ListerMissions.java
@@ -0,0 +1,25 @@
+package dev.mission.exec;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+
+import dev.mission.entite.Mission;
+import dev.mission.repository.MissionRepository;
+
+@Controller
+public class ListerMissions implements Runnable {
+	private MissionRepository missionRepository;
+
+	public ListerMissions(MissionRepository missionRepository) {
+		super();
+		this.missionRepository = missionRepository;
+	}
+
+	@Override
+	public void run() {
+		List<Mission> list = this.missionRepository.findAll();
+		for (Mission ref: list) {
+		System.out.println(ref.getId() +". "+ ref.getLibelle());}
+	}
+}
